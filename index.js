d9ac77ee23e9583543392caaada9f0ae2ca0e93d
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-05-05 01:46:10 +0300

    разделяет сортировку массива и создание разметки

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import gallery from "./gallery-items.js";
-console.log(gallery);
+import photos from "./gallery-items.js";
+// console.log(photos[1]);
 
 // 2. Реализация делегирования на галерее ul.js-gallery
 // и получение url большого изображения.
@@ -23,15 +23,8 @@ const refs = {
   close: document.querySelector('[data-action="close-lightbox"]'),
 };
 // 1. Создание и рендер разметки по массиву данных и предоставленному шаблону.
-const galleryMarkup = createGalleryMarkup(gallery);
-console.log(createGalleryMarkup(gallery[1]));
-refs.gallery.insertAdjacentElement("beforeend", galleryMarkup);
-
-function createGalleryMarkup(images) {
-  return images
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery__item">
+const createGalleryMarkup = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
   <a
     class="gallery__link"
     href="${original}"
@@ -40,23 +33,16 @@ function createGalleryMarkup(images) {
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt="${description}"`
-    )
-    .join("");
-}
-
-{
-  /* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
+      alt="${description}" 
+      />
   </a>
-</li>; */
-}
+</li>
+      `;
+};
+// console.log(createGalleryMarkup(photos[1]));
+
+const createGallery = photos.map((img) => createGalleryMarkup(img)).join("");
+
+console.log(createGallery);
+
+refs.jsGal.insertAdjacentElement("afterbegin", createGallery);
